c74e01cf12e22b77bc00b42802d8ec94e8dae61c
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2018-06-25 22:37:34 +0200

    Updated base code

--- Ex04/mpi.c
+++ Ex04/mpi.c
@@ -3,30 +3,26 @@
 #include <string.h>
 #include "mpi.h"
 
-int MESSAGE_LENGTHS[6] = {1000, 10000, 100000, 1000000, 10000000, 100000000};
+int NUM_MESSAGES = 2;
+//int MESSAGE_LENGTHS[] = {1000, 10000, 100000, 1000000, 10000000, 100000000};
+int MESSAGE_LENGTHS[2] = {10, 100};
 
 int main(int argc, char** argv)
 {
-  // FILE* outputFile = fopen("output.txt", "w");
-
   int i, j;
-  for (i = 0; i < 6; i++)
+  //FILE* outputFile = fopen("output.txt", "w");
+  //int maxMessageLength = MESSAGE_LENGTHS[NUM_MESSAGES - 1];
+
+  for (i = 0; i < NUM_MESSAGES; i++)
   {
-    // Initalize string
-    char* text = malloc(sizeof(char) * MESSAGE_LENGTHS[i]);
-    for (j = 0; j < MESSAGE_LENGTHS[i] - 1; j++)
-      text[j] = 'a';
-
-    printf("Sending a message of size: %d\n", strlen(text));
-    int myID, size, sender = 0, receiver = 1, tag = 99;
-
-    MPI_Status status;
-
     // Initialize the MPI library
     MPI_Init(&argc, &argv);
 
+    MPI_Status status;
+    int rank, size, sender = 0, receiver = 1, tag = 99;
+
     // Get current process ID and total number of processes
-    MPI_Comm_rank(MPI_COMM_WORLD, &myID);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Verify the number of processes
@@ -37,28 +33,35 @@ int main(int argc, char** argv)
       exit (-1);
     }
 
+    printf("Sending a message of size: %d\n", MESSAGE_LENGTHS[i]);
+
+    char* text = malloc(sizeof(char) * MESSAGE_LENGTHS[i]);
+    for (j = 0; j < MESSAGE_LENGTHS[i] - 2; j++)
+      text[j] = 'a';
+    char received = 'r';
+
     // Log the start time
     double startTime = MPI_Wtime();
 
     // If process 0
-    if (myID == 0)
+    if (rank == 0)
     {
-      // strcpy(text, sprintf("Hello there from process %d\n", myID));
-      sprintf(text, "Hello there from process %d\n", myID);
-      MPI_Send(text, strlen(text), MPI_CHAR, receiver, tag, MPI_COMM_WORLD);
+      MPI_Send(text, MESSAGE_LENGTHS[i], MPI_CHAR, receiver, tag, MPI_COMM_WORLD);
+      MPI_Recv(&received, 0, MPI_CHAR, receiver, tag, MPI_COMM_WORLD, &status);
     }
     // If process 1
     else
     {
       MPI_Recv(text, MESSAGE_LENGTHS[i], MPI_CHAR, sender, tag, MPI_COMM_WORLD, &status);
-      printf("Process %d received text: %s\n", myID, text);
+      MPI_Send(&received, 0, MPI_CHAR, sender, tag, MPI_COMM_WORLD);
     }
 
     // Log the end time
     double endTime = MPI_Wtime();
 
+    printf("Process %d received text: %s of length: %d\n", rank, text[0], MESSAGE_LENGTHS[i]);
     printf("Start time: %f | End time: %f | Elapsed time: %f\n", startTime, endTime, endTime - startTime);
-    // fprintf(outputFile, "%f, %f, %f\n", startTime, endTime, endTime - startTime);
+    //fprintf(outputFile, "%d, %f, %f, %f\n", MESSAGE_LENGTHS[i], startTime, endTime, endTime - startTime);
 
     // Free memory
     free(text);
@@ -67,5 +70,5 @@ int main(int argc, char** argv)
     MPI_Finalize();
   }
 
-  // fclose(outputFile);
+  //fclose(outputFile);
 }
--- Ex04/mpi_first.c
+++ Ex04/mpi_first.c
@@ -8,7 +8,7 @@
 int main(int argc, char** argv)
 {
   char text[STR_LEN];
-  int myID, size, sender = 0, receiver = 1, tag = 99;
+  int rank, size, sender = 0, receiver = 1, tag = 99;
 
   MPI_Status status;
 
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
   MPI_Init(&argc, &argv);
 
   // Get current process ID and total number of processes
-  MPI_Comm_rank(MPI_COMM_WORLD, &myID);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   if (size != 2)
@@ -27,16 +27,16 @@ int main(int argc, char** argv)
   }
 
   // If process 0
-  if (myID == 0)
+  if (rank == 0)
   {
-    // strcpy(text, sprintf("Hello there from process %d\n", myID));
-    sprintf(text, "Hello there from process %d\n", myID);
+    // strcpy(text, sprintf("Hello there from process %d\n", rank));
+    sprintf(text, "Hello there from process %d\n", rank);
     MPI_Send(text, strlen(text), MPI_CHAR, receiver, tag, MPI_COMM_WORLD);
   }
   else
   {
     MPI_Recv(text, STR_LEN, MPI_CHAR, sender, tag, MPI_COMM_WORLD, &status);
-    printf("Process %d received text: %s\n", myID, text);
+    printf("Process %d received text: %s\n", rank, text);
   }
 
   // Kill all deamons
