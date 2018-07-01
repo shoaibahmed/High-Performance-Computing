4eedd7eaa94b0ef153d4f9a48b8091e43bfc4ed6
shoaibahmed <m.shoaib_ahmed@hotmail.com>
2018-07-01 04:41:53 +0200

    Performance plot added

--- Ex04/plotUtil.py
+++ Ex04/plotUtil.py
@@ -8,38 +8,52 @@ from matplotlib.collections import PolyCollection
 from matplotlib.collections import LineCollection
 from matplotlib.colors import colorConverter
 
-if len(sys.argv) != 2:
-	print ("Usage: plotUtil.py <Log_file.out>")
+if len(sys.argv) != 3:
+	print ("Usage: plotUtil.py <Same_Node_Log_file.out> <Diff_Node_Log_file.out>")
 	exit(-1)
 
-logFile = sys.argv[1];
+sameNodeLogFile = sys.argv[1]
+diffNodeLogFile = sys.argv[2]
 
-N = []
-elapsedTime = []
-startupTime = 0.0
+def loadFile(fileName):
+	N = []
+	elapsedTime = []
+	startupTime = 0.0
 
-with open(logFile, 'r') as logFile:
-	for line in performanceLogFile:
-		vals = line.split(',')
+	with open(fileName, 'r') as logFile:
+		for line in logFile:
+			vals = line.split(',')
 
-		N.append(int(vals[0]))
-		elapsedTime.append(float(vals[-1]))
-		startupTime = float(vals[-2])
+			N.append(int(vals[0]))
+			elapsedTime.append(float(vals[-1]))
+			startupTime = float(vals[-2])
 
-print (N)
-print (elapsedTime)
-print (startupTime)
-exit(0)
+	# Add the zero-point
+	elapsedTime = [startupTime] + elapsedTime
+	N = [0] + N
 
-ax.plot(numAdditions, kernelAPerformance, 'r', label='Performance (Kernel A)', linewidth=2.0)
-ax.set_xlabel('Number of add operations')
-ax.set_ylabel('Performance (GFlops)')
-plt.savefig('./Task0' + str(task) + '_KernelA_Performance.png', dpi=300)
+	return N, elapsedTime
+
+N_SN, elapsedTime_SN = loadFile(sameNodeLogFile)
+N_DN, elapsedTime_DN = loadFile(diffNodeLogFile)
+
+print (N_SN, elapsedTime_SN)
+print (N_DN, elapsedTime_DN)
 
 fig, ax = plt.subplots()
-ax.set_title('Bandwidth (Kernel M) - Occupancy: ' + occupancy[task-2])
+fig.set_size_inches(18.5, 10.5)
+ax.set_title('MPI Performance Plot', color='C0')
 
-ax.plot(numMemoryOperations, kernelMBandwidth, 'r', label='Bandwidth (Kernel M)', linewidth=2.0)
-ax.set_xlabel('Number of memory operations')
-ax.set_ylabel('Bandwidth (GB/s)')
-plt.savefig('./Task0' + str(task) + '_KernelM_Bandwidth.png', dpi=300)
+ax.plot(N_SN, elapsedTime_SN, linestyle='--', marker='o', color='b', label='Same Node', linewidth=2.0)
+ax.plot(N_DN, elapsedTime_DN, linestyle='--', marker='o', color='g', label='Different Nodes', linewidth=2.0)
+
+ax.legend()
+
+ax.set_xlabel('Message length (Bytes)')
+ax.set_ylabel('Time (Seconds)')
+
+outputPath = "./performancePlot.png"
+plt.tight_layout()
+plt.savefig(outputPath, dpi=300)
+plt.show()
+plt.close('all')
